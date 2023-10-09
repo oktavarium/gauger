fafcc8a5c0cb9030041cb656e172da515a33af46
okta <v_denisenko@eureca.ru>
2023-10-09 18:28:06 +0300

    fix updatecounter sql query

--- internal/server/internal/gaugeserver/internal/storage/internal/pg/update.go
+++ internal/server/internal/gaugeserver/internal/storage/internal/pg/update.go
@@ -16,12 +16,12 @@ func (s *storage) UpdateCounter(ctx context.Context, name string, val int64) (in
 	row := tx.QueryRowContext(ctx, `
 		INSERT INTO counter (name, value) VALUES ($1, $2)
 		ON CONFLICT (name) DO
-		UPDATE counter SET value = counter.value + %2
+		UPDATE SET value = counter.value + %2
 		RETURNING value`, name, val)
 
 	err = row.Scan(&newValue)
 	if err != nil {
-		return 0, fmt.Errorf("error occured on inserting gauge: %w", err)
+		return 0, fmt.Errorf("error occured on updating counter: %w", err)
 	}
 
 	return newValue, tx.Commit()
