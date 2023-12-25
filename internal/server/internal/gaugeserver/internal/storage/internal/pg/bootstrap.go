73aee296399988c901eac288b3b49433b650e2f7
okta <van.denisenko@gmail.com>
2023-12-25 23:35:48 +0300

    add endpoint example

--- internal/server/internal/gaugeserver/internal/storage/internal/pg/bootstrap.go
+++ internal/server/internal/gaugeserver/internal/storage/internal/pg/bootstrap.go
@@ -26,30 +26,24 @@ counter
 `
 
 func (s *storage) bootstrap(ctx context.Context) (err error) {
-	fmt.Println("!")
 	tx, err := s.BeginTx(ctx, pgx.TxOptions{})
-	fmt.Println("!!")
 	if err != nil {
-		fmt.Println("!!!")
 		return fmt.Errorf("error occured on opening tx: %w", err)
 	}
-	fmt.Println("!!!!")
+
 	defer func() {
 		if err != nil {
-			fmt.Println("!!!!!")
 			err = tx.Rollback(ctx)
 		} else {
-			fmt.Println("!!!!!!!!!")
 			err = tx.Commit(ctx)
 		}
 
 	}()
-	fmt.Println("!!!!!!")
+
 	_, err = tx.Exec(ctx, createTablesQuery)
 	if err != nil {
-		fmt.Println("!!!!!!!")
 		return fmt.Errorf("error occured on creating tables: %w", err)
 	}
-	fmt.Println("!!!!!!!!")
+
 	return nil
 }
