80cbb449d86caee1f352870e8e3eeeedfc73cda5
okta <van.denisenko@gmail.com>
2023-12-25 23:36:53 +0300

    remove pprof server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,19 +3,10 @@ package main
 import (
 	"fmt"
 
-	// _ "net/http/pprof"
-
 	"github.com/oktavarium/go-gauger/internal/server"
 )
 
 func main() {
-	// func() {
-	// 	err := http.ListenAndServe(":8888", nil)
-	// 	if err != nil {
-	// 		panic(fmt.Errorf("pprf error: %w", err))
-	// 	}go
-	// }()
-
 	if err := server.Run(); err != nil {
 		panic(fmt.Errorf("error on running server: %w", err))
 	}
