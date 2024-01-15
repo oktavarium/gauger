c78abb67c969ed088310dfa75780c8e958541136
okta <van.denisenko@gmail.com>
2024-01-15 22:44:29 +0300

    iter20

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,8 +6,33 @@ import (
 	"github.com/oktavarium/go-gauger/internal/server"
 )
 
+var buildVersion string
+var buildDate string
+var buildCommit string
+
+const NA string = "N/A"
+
 func main() {
+	printBuildInfo()
 	if err := server.Run(); err != nil {
 		panic(fmt.Errorf("error on running server: %w", err))
 	}
+}
+
+func printBuildInfo() {
+	if len(buildVersion) == 0 {
+		buildVersion = NA
+	}
+
+	if len(buildDate) == 0 {
+		buildDate = NA
+	}
+
+	if len(buildCommit) == 0 {
+		buildCommit = NA
+	}
+
+	fmt.Printf(
+		"Build version: %s\n Build data: %s\n Build commit: %s\n",
+		buildVersion, buildDate, buildCommit)
 }
